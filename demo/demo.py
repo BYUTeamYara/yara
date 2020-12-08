77d3e9f3b464628e4a4040069e16f6e4a12bf059
Max <maxpark@cilantro.localdomain>
2020-12-08 09:37:08 -0700

    added some comments to the python demo

--- demo/demo.py
+++ demo/demo.py
@@ -1,4 +1,5 @@
 #Runs Yara and Hyperscan multiple times to compare.
+#Written by Max Parkinson and Jackson Flood
 
 import os
 import time
@@ -6,6 +7,9 @@ import time
 runTimes=10
 totalTime=0
 
+#runs Yara number of times and gets average time.
+#test.yara can be edited depending on the rule that you want to use.
+print("Testing Yara...")
 for i in range(runTimes):
     start=time.time()
     os.system("yara test.yara syslog | grep null")
@@ -13,9 +17,11 @@ for i in range(runTimes):
     totalTime+=end-start
 
 totalTime=totalTime/runTimes
-print("Average Yara time: " + str(totalTime) + " seconds.")
+print("Average Yara time:         " + str(totalTime) + " seconds.")
 totalTime=0
 
+#runs Hyperscan simplegrep program and gets average time.
+print("Testing Hyperscan...")
 for i in range(runTimes):
     start=time.time()
     os.system("./simplegrep C3HQS750B syslog | grep null")
@@ -23,4 +29,4 @@ for i in range(runTimes):
     totalTime+=end-start
 
 totalTime=totalTime/runTimes
-print("Average Hyperscan time: " + str(totalTime) + " seconds.")
+print("Average Hyperscan time:    " + str(totalTime) + " seconds.")
